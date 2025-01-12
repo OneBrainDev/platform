84a0fcf5dc5ca226822b056f55f5247a2073ff92
Joe Cianflone <JoeCianflone@users.noreply.github.com>
2025-01-12 10:11:27 -0500

    feature: wip of vue

--- frontend/web/vite.config.ts
+++ frontend/web/vite.config.ts
@@ -1,12 +1,45 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import  laravel  from 'laravel-vite-plugin'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+// export default defineConfig({
+// 	envDir: '../../application',
+// 	server: {
+// 		host: 'https://platform.test',
+// 		port: 3000,
+// 		hmr: {
+// 			host: 'platform.test'
+// 		},
+// 		watch: {
+// 			usePolling: true
+// 		}
+// 	},
+// 	plugins: [
+// 		laravel({
+// 			detectTls: 'platform.test',
+// 			input: 'app.js',
+// 			refresh: true,
+// 			publicDirectory: '../../application/public'
+// 		}),
+// 		svelte(),
+// 		basicSsl()
+// 	]
+// });
+
 // https://vite.dev/config/
 export default defineConfig({
-   plugins: [vue(), vueDevTools()],
+   plugins: [
+		laravel({
+			detectTls: 'platform.test',
+			input: 'app.js',
+			refresh: true,
+			publicDirectory: '../../application/public'
+		}),
+      vue(),
+      vueDevTools()
+   ],
    resolve: {
       alias: {
          '@': fileURLToPath(new URL('./src', import.meta.url)),
