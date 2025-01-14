874e750afb7b1d607266d47a867238bf1090305e
Joe Cianflone <JoeCianflone@users.noreply.github.com>
2025-01-14 10:52:05 -0500

    fix: docker env example updated (#34)

--- frontend/ds/src/components/DSGroup/DSGroup.spec.ts
+++ frontend/ds/src/components/DSGroup/DSGroup.spec.ts
@@ -5,15 +5,44 @@ import DsGroup from './DsGroup.vue'
 
 describe('DSGroup', () => {
    it('renders a default stack', () => {
-      const wrapper = mount(DsGroup, {})
+      const wrapper = mount(DsGroup, {
+         props: {
+            type: 'stack',
+            gap: 'xsmall',
+            inline: 'start',
+            block: 'start',
+            wrap: false,
+         }
+      })
       expect(wrapper.classes()).toContain('ds-group')
       expect(wrapper.classes()).toContain('stack')
+   })
+
+   it('renders the main slot', () => {
+      const wrapper = mount(DsGroup, {
+         props: {
+            type: 'stack',
+            gap: 'xsmall',
+            inline: 'start',
+            block: 'start',
+            wrap: false,
+         },
+         slots: {
+            default: "Main Content"
+         }
+      })
+
+      expect(wrapper.html()).toContain('Main Content')
    })
 
    it('renders a cluster', () => {
       const wrapper = mount(DsGroup, {
          props: {
-            type: 'cluster'
+            type: 'cluster',
+            gap: 'xsmall',
+            inline: 'start',
+            block: 'start',
+            wrap: false,
          }
       })
       expect(wrapper.classes()).toContain('ds-group')
