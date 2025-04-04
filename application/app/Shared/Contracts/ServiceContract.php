fa6c972538d11a1fb55bd9f2e7ca2a53bcad807f
Joe Cianflone <JoeCianflone@users.noreply.github.com>
2025-04-04 14:18:24 -0400

    feature: add submodules for shared platform code (#53)
    * feature: removes folders that are now submodules
    
    * feature: adds submodules for code that is shared

--- application/app/Shared/Contracts/ServiceContract.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Platform\Shared\Contracts;
-
-interface ServiceContract
-{
-    public function handle(DataObjectContract $data): mixed;
-}
--- application/tests/Architecture/GeneralArchitectureTest.php
+++
@@ -1,5 +0,0 @@
-<?php declare(strict_types=1);
-
-arch()->preset()->php();
-arch()->preset()->security()->ignoring('shell_exec');
-arch()->preset()->laravel();
