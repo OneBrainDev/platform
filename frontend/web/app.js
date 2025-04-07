fff69e2bbf59796f8cf0ba17bcb3156b76aff3b3
Joe Cianflone <JoeCianflone@users.noreply.github.com>
2025-04-06 20:33:01 -0400

    feature: add in wayfinder and removes ziggy (#56)

--- frontend/web/app.js
+++ frontend/web/app.js
@@ -1,8 +1,6 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 
-import { ZiggyVue } from 'ziggy-js'
-
 import '@platform/blank/dist/blank.css'
 
 createInertiaApp({
@@ -13,7 +11,6 @@ createInertiaApp({
    setup({ el, App, props, plugin }) {
       createApp({ render: () => h(App, props) })
          .use(plugin)
-         .use(ZiggyVue)
          .mount(el)
    },
 })
